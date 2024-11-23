723f449a997683ba4564335b081f905b421f3d32
Tom BRUAIRE <tombruaire@gmail.com>
2024-11-23 21:05:44 +0100

    Importation des assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'nom-du-projet';
